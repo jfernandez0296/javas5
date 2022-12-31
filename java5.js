91e4b0a0e16db8bd01330a63b9ea40279c58ebd4
jfernandez0296 <60338655+jfernandez0296@users.noreply.github.com>
2022-12-31 18:23:25 -0500

    Add files via upload

---
+++ java5.js
@@ -0,0 +1,38 @@
+// Combinar dos objetos con claves iguales
+
+function myFunction(objX, objY) {
+    const objz={...objX,...objY};
+    for(i in objX)
+    {for (j in objY)
+        {if( i==j)
+        {objz[i]=objX[i];}
+        }
+    }
+    return objz;
+  }
+
+  console.log(myFunction({a:1,b:2,c:3},{a:4,d:5,e:6}))
+
+// Eliminar una propiedad de un objeto
+
+function myFunction(obj, key) {
+    delete obj[key];
+    return  obj
+  } 	
+
+
+console.log(myFunction({a:1,b:2,c:3},'b'));
+
+// Sumar valores de un objeto
+
+function myFunction(obj) {
+    let sum=0;
+    for(let i in obj)
+        {sum+=obj[i];}
+
+    return sum
+  }
+
+  console.log(myFunction({a:1,b:2,c:3}))
+
+
